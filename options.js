d4336a4fef7f9088acddee709631f3e701f9b52c
Chris Priest <cp368202@ohiou.edu>
2015-01-10 12:36:22 -0500

    got bitcoinjs working

--- options.js
+++ options.js
@@ -21,14 +21,27 @@ function save_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-    // Use default value color = 'red' and likesColor = true.
+    // These are default values
     chrome.storage.sync.get({
         when_to_send: '5min',
-        tip_amount: 0.0001
+        tip_amount: 0.05,
+        public_key: 'none',
+        private_key: 'none',
     }, function(items) {
         console.log(items);
+        if(items.public == 'none' && items.private_key == 'none') {
+            //if keys have not been generated, do so now and save them.
+            var key = Bitcoin.ECKey.makeRandom();
+            items.pubkey = key.pub.getAddress().toString();
+            items.priv_key = key.toWIF();
+            chrome.storage.sync.set({
+                pub_key: items.pub_key,
+                priv_key: items.priv_key
+            }
+        }
         $('input[name=when_to_send][value=' + items.when_to_send + ']').attr('checked', 'checked');
         $('input[name=tip_amount]').val(items.tip_amount);
+        $('#deposit_address').text(items.pub_key);
     });
 }
 
